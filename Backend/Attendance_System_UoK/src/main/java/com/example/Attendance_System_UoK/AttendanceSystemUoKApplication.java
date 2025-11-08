aeabf41fb0b7b5ee7898395f5d899ad6eb02f07f
prasa200204 <madhuma-ec21071@stu.kln.ac.lk>
2025-11-08 23:44:26 +0530

    spring boot project setup

---
+++ Backend/Attendance_System_UoK/src/main/java/com/example/Attendance_System_UoK/AttendanceSystemUoKApplication.java
@@ -0,0 +1,13 @@
+package com.example.Attendance_System_UoK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceSystemUoKApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendanceSystemUoKApplication.class, args);
+	}
+
+}
---
+++ Backend/Attendance_System_UoK/src/test/java/com/example/Attendance_System_UoK/AttendanceSystemUoKApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Attendance_System_UoK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceSystemUoKApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
