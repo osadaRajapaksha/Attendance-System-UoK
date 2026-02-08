c986c08717267185d18879e00ae9e5cf36b39a6e
prasa200204 <madhuma-ec21071@stu.kln.ac.lk>
2026-02-08 18:51:17 +0530

    baseurl change

--- frontend_react/src/api/axios.ts
+++ frontend_react/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080', // Adjust if backend runs on different port
+  baseURL: 'https://api.attendancesystemuok.me', // Adjust if backend runs on different port
 });
 
 api.interceptors.request.use(
--- frontend_react/vite.config.ts
+++ frontend_react/vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    https: false,
-    host: true
+    host: "0.0.0.0",
+    port: 5173,
+    strictPort: true,
+    allowedHosts: [
+      ".attendancesystemuok.me"
+    ]
   }
-})
+});
